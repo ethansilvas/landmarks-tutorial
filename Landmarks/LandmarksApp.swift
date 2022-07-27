fd1eee34673dea0706bb56fc258cb18bc25bd953
Ethan Silvas <ethansilvas2@gmail.com>
2022-07-27 13:27:18 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ethan Silvas on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
