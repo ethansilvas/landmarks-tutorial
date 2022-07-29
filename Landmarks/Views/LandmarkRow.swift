28718754301e38dc96f794571fc627574b5c178d
Ethan Silvas <ethansilvas2@gmail.com>
2022-07-29 12:04:17 -0700

    create row view

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 
 struct LandmarkRow: View {
+    var landmark: Landmark
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
     }
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkRow()
+        LandmarkRow(landmark: landmarks[0])
     }
 }
